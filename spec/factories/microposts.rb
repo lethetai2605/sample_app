ac972dd9722e81136a3a210ceb58d7d4e50454de
letai <thetai2605@gmail.com>
2022-02-10 09:18:31 +0700

    update1

--- spec/factories/microposts.rb
+++ spec/factories/microposts.rb
@@ -1,16 +1,4 @@
 FactoryBot.define do
-  factory :user do
-    sequence(:email) { |n| "#{n}@example.com" }
-    password { "12345678" }
-    password_confirmation { "12345678" }
-    name { "tai123" }
-    activated { true }
-    read_notification { false }
-    created_at { Time.zone.now }
-    confirmed_at { Time.zone.now }
-    activated_at { Time.zone.now }
-  end
-
   factory :micropost do
     user
     content { "test factory bot" }
